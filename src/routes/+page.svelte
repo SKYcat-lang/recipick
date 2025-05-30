31fa6936523c7e8c8d35b9b8219a84855595726a
SKSKIe <gahee3074@gmail.com>
2025-05-31 03:21:52 +0900

    프로젝트 업

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,35 @@
 <Header />
 <style>
     main {
-        background-image: url('/background.jpeg'); /* static 폴더의 이미지는 /경로로 접근 */
+        background-image: url('/background.png'); /* static 폴더의 이미지는 /경로로 접근 */
         background-size: cover; /* 화면 전체를 덮도록 설정 */
         background-position: center center; /* 이미지를 중앙에 위치 */
         background-repeat: no-repeat; /* 이미지 반복 없음 */
         background-attachment: fixed; /* 스크롤 시 배경 이미지가 고정됨 (선택 사항) */
     }
+  /* 부트스트랩 shadow-sm 클래스에 hover 효과를 추가하는 CSS */
+
+  .a-hover {
+    color: gray; /* 부트스트랩의 large shadow 변수 사용 */
+  }
+
+  .a-hover:hover {
+    color: black; /* 부트스트랩의 large shadow 변수 사용 */
+  }
 </style>
 <main>
-    <div class="container py-3">
+    <div class="container">
         <div class="row">
-            <div class="col-7">
-                <h1>홈페이지</h1>
-                <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+            <div class="col-xl-7 py-xl-5">
+                <div class="container auto py-5 px-md-5">
+                    <div class="text-white py-xl-4"><b>Discover Dellcious Recipes</b> - 맛있는 요리법을 발견하세요.</div>
+                    <div class="py-md-1 py-xl-5">
+                        <h1 class="text-white display-4 fw-medium">레시피로 가득한</h1>
+                        <h1 class="px-md-4 text-white display-4 fw-medium">맛있는 여정</h1>
+                    </div>
+                </div>
             </div>
-            <div class="col-5">
+            <div class="col-xl-5">
                 <div class="modal modal-sheet position-static d-block p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
                     <div class="modal-dialog">
                         <div class="modal-content rounded-4 shadow">
@@ -40,9 +54,9 @@
                             <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
                             <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
                             <div class="py-2 mb-3 form-check d-flex">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">로그인 정보 저장</label>
-                                <a class="ms-auto text-dark text-opacity-50 text-decoration-none" href="#">회원가입</a>
+                                <input type="checkbox" class="form-check-input" id="floatingCheck">
+                                <label class="form-check-label px-2" for="floatingCheck">로그인 정보 저장</label>
+                                <a class="ms-auto text-decoration-none px-2 a-hover" href="#">회원가입</a>
                             </div>
                             <hr class="my-4">
                             <h2 class="fs-5 fw-bold mb-3">다른 방식으로 로그인</h2>
